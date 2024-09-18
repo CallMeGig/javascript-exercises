29013e32f080aed22b1e1ea21c73f31932edd17d
Ghislain <73262104+CallMeGig@users.noreply.github.com>
2024-09-18 15:47:14 +0200

    Add logic to repeat a given string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	let workingString = "";
+	if (num < 0) {
+		workingString = "ERROR";
+	} else {
+		for (let i = 0; i < num; i++) {
+			workingString += string;
+		}
+	}
+	return workingString;
 };
 
 // Do not edit below this line
