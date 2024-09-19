8928a51a1fea233fcecca6901f0f529dd91d4f92
Ghislain <73262104+CallMeGig@users.noreply.github.com>
2024-09-19 22:24:34 +0200

    Add logic to return the sum of every integer between 2 positive integers inclusively

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+	let smallerNum = Math.min(num1, num2);
+	let largerNum = Math.max(num1, num2);
+	let result = 0;
+	
+	if (smallerNum < 0) {
+		return "ERROR";
+	} else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+		return "ERROR";
+	}
 
+	for (let i = smallerNum; i <= largerNum; i++) {
+		result += i;
+	}
+	return result;
 };
 
 // Do not edit below this line
