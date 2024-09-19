47c58d29f53dba1f0e96bb7e5ce5ca368462543c
Ghislain <73262104+CallMeGig@users.noreply.github.com>
2024-09-19 13:44:18 +0200

    Add logic to remove n args from a given array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+	let result = [];
+	const numsToRemove = Array.from(arguments).slice(1);
+	for (value of arr) {
+		if (numsToRemove.includes(value)) {
+			continue;
+		} else {
+			result.push(value);
+		}
+	}
+	return result;
 };
 
 // Do not edit below this line
