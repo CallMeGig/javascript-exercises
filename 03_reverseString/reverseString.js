1c6e1096a1cb13de5fb71d6281fd77c1775762a5
Ghislain <73262104+CallMeGig@users.noreply.github.com>
2024-09-20 14:31:26 +0200

    Add logic to return a provided string in reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	const stringArray = string.split("");
+	let result = "";
 
+	for (let i = string.length-1; i >=0; i--) {
+		result += stringArray[i];
+	}
+	return result;
 };
 
 // Do not edit below this line
